806a63462bbf43dc1000f4e7defe3312c2525c36
Heesoo Hwang <email.academic>
2026-01-31 21:45:30 +0900

    feat: removed calculateAge and calculateUrgencyScore

--- src/api/v1/services/ticketServices.ts
+++ src/api/v1/services/ticketServices.ts
@@ -121,29 +121,3 @@ export const updateTicket = (id: number, newPriority?: ticketPriority, newStatus
        
        return ticket;
 }
-
-export const calculateAge = (id: number): number => {
-       const ticket = getTicketById(id);
-       if(ticket === null){
-              return 0;
-       }
-       
-       const ageInMs = Date.now() - new Date(ticket.createdAt).getTime();
-       const ageInDays = Math.floor(ageInMs / (1000 * 60 * 60 * 24));
-
-       return ageInDays;
-}
-
-export const calculateUrgencyScore = (id: number): number | null => {
-       const ticket = getTicketById(id);
-       if(ticket === null){
-              return null;
-       }
-
-       const multiplier: number = 5;
-       const age: number = calculateAge(id);
-       
-       const urgencyScore = BASE_URGENCY[ticket.priority] + (age * multiplier);
-       
-       return urgencyScore;
-}
