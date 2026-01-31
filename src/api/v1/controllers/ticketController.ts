161dbddc4eca42bc4c62a63e89eddf8c1558a18a
Heesoo Hwang <email.academic>
2026-01-31 17:58:51 +0900

    feat: added getAllTickets controller

---
+++ src/api/v1/controllers/ticketController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+export const getAllTickets = (req: Request, res: Response) => {
+       //call the service function to get all the tickets
+       res.json();
+
+}
